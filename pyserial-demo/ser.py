0393addda0fd1c9f7d3c2856dec7943bd7ec1549
handy <handy8831505@gmail.com>
2016-07-20 14:58:07 +0800

    modify ser.py for excute in linux/mint

--- pyserial-demo/ser.py
+++ pyserial-demo/ser.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 import serial
 
 def main():
     ser = serial.Serial("/dev/ttyftdi")
     ser.baudrate = 9600
-    ser.write("abcdefghijklmnopqrstuvwxyz");
+    ser.write("abcdefghijklmnopqrstuvwxyz".encode());
     ser.close()
 
 if __name__ == "__main__":
