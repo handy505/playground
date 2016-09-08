42025d81a8c651a6723da5b62a017c4669056054
handy <handy8831505@gmail.com>
2016-09-08 11:06:12 +0800

    fixbug hhhsaver doesn't save arrived.log

--- python/hhsaver/hhsaver.py
+++ python/hhsaver/hhsaver.py
@@ -22,7 +22,7 @@ def get_last_record(filename):
                 last_record = line.rstrip("\n")        
     else:
         print("file not exist, create it.")
-        with open("arrived.log", "w") as f:
+        with open(LOGFILE, "w") as f:
             rec = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(round(time.time())))
             f.write(rec + "\n")
             last_record = rec
@@ -50,7 +50,7 @@ if __name__ == '__main__':
         try:
             last_record = get_last_record(LOGFILE)
             lc2 = time.strptime(last_record, '%Y-%m-%d %H:%M:%S')
-            print("ts2-arrived: " + time.strftime('%Y-%m-%d %H:%M:%S', lc2) )            
+                     
         except ValueError:
             print(ValueError)
             exit()
@@ -59,16 +59,17 @@ if __name__ == '__main__':
         the_same_day = ((lc1.tm_year == lc2.tm_year) and (lc1.tm_mon == lc2.tm_mon) and (lc1.tm_mday == lc2.tm_mday)) 
         if not the_same_day:
             print("a new day")
-            with open("arrived.log", "a") as f:
+            with open(LOGFILE, "a") as f:
                 f.write( time.strftime('%Y-%m-%d %H:%M:%S', lc1) + "\n")
+            lc2 = lc1
 
         # calculate timestamp
         t2 = time.mktime(lc2)
         t3 = t2 + (60*60*9)
         lc3 = time.localtime(t3)
-        print("ts3-freedom: " + time.strftime('%Y-%m-%d %H:%M:%S', lc3) )
+        print("ts2-arrived: " + time.strftime('%Y-%m-%d %H:%M:%S', lc2) )   
         print("ts1-current: " + time.strftime('%Y-%m-%d %H:%M:%S', lc1) )
-
+        print("ts3-freedom: " + time.strftime('%Y-%m-%d %H:%M:%S', lc3) )
         hour, min, sec = odd_hms(t3-t1)
         print("get off work: {}:{}:{}".format(hour, min, sec))
 
