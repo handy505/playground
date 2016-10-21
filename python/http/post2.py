755264b49618b595eaf710287be12dbd293a37bd
handy <handy8831505@gmail.com>
2016-10-21 17:21:43 +0800

    fix upload thread post string, post minute ok

--- python/http/post2.py
+++ python/http/post2.py
@@ -10,9 +10,7 @@ import json
 2) 使用 posttestserver.com 提供的服務做驗証
 '''
 
-poststr = """
-InverterData@[{"UniqueID": "40337", "InverterID": "1", "Expr1002": "2016-10-21 10:48:38", "OutputPower": "120", "ACVolPhaseA": "119", "ACVolPhaseB": "120", "ACVolPhaseC": "112", "ACFrequency": "114", "ACOutputCurrentA": "103", "ACOutputCurrentB": "108", "ACOutputCurrentC": "105", "DC1InputVol": "119", "DC2InputVol": "118", "DC1InputCurrent": "111", "DC2InputCurrent": "100", "DCBusPositiveVol": "101", "DCBusNegativeVol": "100", "InternalTemper": "119", "HeatSinkTemper": "100", "InputPowerA": "115", "InputPowerB": "113", "TotalOutputPower": "4207"}]
-"""
+poststr = """InverterData@[{"UniqueID": "40337", "InverterID": "1", "Expr1002": "2016-10-21 10:48:38", "OutputPower": "120", "ACVolPhaseA": "119", "ACVolPhaseB": "120", "ACVolPhaseC": "112", "ACFrequency": "114", "ACOutputCurrentA": "103", "ACOutputCurrentB": "108", "ACOutputCurrentC": "105", "DC1InputVol": "119", "DC2InputVol": "118", "DC1InputCurrent": "111", "DC2InputCurrent": "100", "DCBusPositiveVol": "101", "DCBusNegativeVol": "100", "InternalTemper": "119", "HeatSinkTemper": "100", "InputPowerA": "115", "InputPowerB": "113", "TotalOutputPower": "4207"}]"""
 
 #url = "http://posttestserver.com/post.php"
 url = "http://solar.ablerex.com.tw/DataStorage/DataStorage.ashx?psid=290"
