c2494ca11a285f4052fe22a34484d851f66c0c67
handy <handy8831505@gmail.com>
2016-12-29 16:42:23 +0800

    ble socket, command send/response ok

--- python/socket_bridge_ble/client.py
+++ python/socket_bridge_ble/client.py
@@ -4,6 +4,7 @@
 import socket
 import sys
 import time
+import os
 
 HOST = 'localhost'
 PORT = 12345
@@ -17,9 +18,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as so:
         data = so.recv(1024)
         if data:
             s = data.decode('utf-8')
-            print('\t' + s)
+            print('\t [client recv] ' + s)
 
-            s = 'response data'
-            bs = bytearray(s.encode('utf-8'))
+            #s = 'response data'
+            rs = os.popen(s).read()
+            bs = bytearray(rs.encode('utf-8'))
             so.sendall(bs)
-            print(s)
+            print('[client send] ' + bs.decode('utf-8'))
--- python/socket_bridge_ble/server.py
+++ python/socket_bridge_ble/server.py
@@ -12,13 +12,14 @@ def recv_process(conn, addr):
         data = conn.recv(1024)
         if data:
             s = data.decode('utf-8')
-            print('\t' + s)
+            print('\t[server recv] ' + s)
 
 
 HOST = 'localhost'
 PORT = 12345
 
-commands = ['ifconfig wlan0', 'hostname', 'date']
+#commands = ['ifconfig wlan0', 'hostname', 'date']
+commands = ['hostname', 'date']
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as so:
     so.bind((HOST, PORT))
@@ -31,21 +32,16 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as so:
             
             rt = threading.Thread(target=recv_process, args=(conn, addr))
             rt.start()
-            last = time.time()
+            
             while True:
                 
-                if time.time() - last > 3:
-                    '''ts = time.strftime('%H:%M:%S', time.localtime(round(time.time())))
-                    s = ts + ' ' + commands[random.randint(0,2)]'''
-                    bs = bytearray(s.encode('utf-8'))
-                    conn.sendall(bs)
-                    print(ts)
-                    last = time.time()
+                s = commands[random.randint(0, len(commands)-1)]
+                bs = bytearray(s.encode('utf-8'))
+                conn.sendall(bs)
+                print('[server send]' + bs.decode('utf-8'))
 
-                    '''data = conn.recv(1024)
-                    if data:
-                        s = data.decode('utf-8')
-                        print('\t' + s)'''
+                time.sleep(2)
+        
 
     except socket.error as err:
         print(err)
