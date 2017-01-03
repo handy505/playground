286f7b387fc32f381e8a86752e3904272ba16541
handy <handy8831505@gmail.com>
2017-01-03 17:37:23 +0800

    add ConnectionRefuseError exception process in socket client

--- python/socket_bridge_ble/client.py
+++ python/socket_bridge_ble/client.py
@@ -8,20 +8,24 @@ import os
 
 HOST = 'localhost'
 PORT = 12345
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as so:
 
-    so.connect((HOST, PORT))
+try:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as so:
 
-    while True:
+        so.connect((HOST, PORT))
 
+        while True:
 
-        data = so.recv(1024)
-        if data:
-            s = data.decode('utf-8')
-            print('\t [client recv] ' + s)
+            data = so.recv(1024)
+            if data:
+                s = data.decode('utf-8')
+                print('\t [client recv] ' + s)
 
-            #s = 'response data'
-            rs = os.popen(s).read()
-            bs = bytearray(rs.encode('utf-8'))
-            so.sendall(bs)
-            print('[client send] ' + bs.decode('utf-8'))
+                #s = 'response data'
+                rs = os.popen(s).read()
+                bs = bytearray(rs.encode('utf-8'))
+                so.sendall(bs)
+                print('[client send] ' + bs.decode('utf-8'))
+except ConnectionRefusedError as err:
+    print(err)
+    
