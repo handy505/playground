3c736a1ef0006de669f0feaab5c30da3ea68397d
handy <handy8831505@gmail.com>
2017-03-28 17:14:47 +0800

    mcs upload data success

--- python/mcs/mcs_upload.py
+++ python/mcs/mcs_upload.py
@@ -2,13 +2,23 @@
 # -*- coding: utf-8 -*-
 
 from urllib.request import Request, urlopen
+import requests
+
+
+deviceId = 'DoDRf1I3'
+deviceKey = 'LThU2YXaK8RETiXd'
 
 if __name__ == '__main__':
 
-    url = 'https://api.mediatek.com/mcs/v2/devices/DoDRf1I3/connections.csv'
-    
-    r = Request(url)
-    r.add_header('DeviceKey', 'LThU2YXaK8RETiXd')
-    
-    recv_str = urlopen(r).read()
-    print(recv_str)
+    # 向server拿長連接的ip:port
+    url = 'https://api.mediatek.com/mcs/v2/devices/{deviceId}/connections.csv'.format(deviceId=deviceId)
+    headers={'DeviceKey': deviceKey, 'Content-Type':'text/csv'}
+    resp = requests.get(url, headers=headers)
+    print(resp.text)
+
+    # 維持長連接，未實做
+
+    # 上傳
+    url = 'https://api.mediatek.com/mcs/v2/devices/{deviceId}/datapoints.csv'.format(deviceId=deviceId)
+    r = requests.post(url, data='1,,26.34', headers=headers)
+    print(r.text)
