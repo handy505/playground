e8a72aa4997057432affc47c79ab2bf280b7e98e
handy <handy8831505@gmail.com>
2017-03-28 17:47:23 +0800

    mcs read data point ok

--- python/mcs/mcs_upload.py
+++ python/mcs/mcs_upload.py
@@ -11,14 +11,24 @@ deviceKey = 'LThU2YXaK8RETiXd'
 if __name__ == '__main__':
 
     # 向server拿長連接的ip:port
-    url = 'https://api.mediatek.com/mcs/v2/devices/{deviceId}/connections.csv'.format(deviceId=deviceId)
+    url = 'https://api.mediatek.com/mcs/v2/devices/{}/connections.csv'.format(deviceId)
     headers={'DeviceKey': deviceKey, 'Content-Type':'text/csv'}
     resp = requests.get(url, headers=headers)
     print(resp.text)
 
-    # 維持長連接，未實做
 
-    # 上傳
-    url = 'https://api.mediatek.com/mcs/v2/devices/{deviceId}/datapoints.csv'.format(deviceId=deviceId)
-    r = requests.post(url, data='1,,26.34', headers=headers)
+    # 維持長連接，未實做
+    # ...
+
+
+    # 上傳資料點
+    url = 'https://api.mediatek.com/mcs/v2/devices/{}/datapoints.csv'.format(deviceId)
+    r = requests.post(url, data='1,,33.34', headers=headers)
     print(r.text)
+
+
+    # 讀取資料點
+    deviceChn = '1'
+    url = 'https://api.mediatek.com/mcs/v2/devices/{}/datachannels/{}/datapoints.csv'.format(deviceId, deviceChn)
+    resp = requests.get(url, headers=headers)
+    print(resp.text)
