93629f6d6d4ff092686834e8b0b9c2a33a66ee48
handy <handy8831505@gmail.com>
2017-09-19 13:45:06 +0800

    add logging demo

--- python/logging/logging_demo.py
+++ python/logging/logging_demo.py
@@ -2,15 +2,30 @@
 # -*- coding:utf-8 -*-
 
 import logging
+import submod
+
+
+logger = logging.getLogger()
+#logging.basicConfig(filename='handy.log', level=logging.WARN)
+handler = logging.StreamHandler()
+handler.setLevel(logging.DEBUG)
+logger.addHandler(handler)
+
+handler = logging.FileHandler('handy.log')
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
 
 def main():
-    logger = logging.getLogger('firstlogging')
-    logging.basicConfig(level=logging.WARN)
 
-    logger.debug('debug')
-    logger.info('info')
-    logger.warning('warn')
-    logger.error('error')
+
+    logger.debug('debug message')
+    logger.info('info message')
+    logger.warning('warn message')
+    logger.error('error message')
+
+    submod.subfunc()
 
 if __name__ == '__main__':
     main()
