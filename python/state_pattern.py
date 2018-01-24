5407952d753fdab778e936774956dfc15317a5a3
handy <handy8831505@gmail.com>
2018-01-24 18:08:05 +0800

    rewrite ./python

--- python/state_pattern.py
+++ python/state_pattern.py
@@ -1,37 +1,39 @@
 #!/usr/bin/env python3
-'''
+
+MESSAGE = '''\
 State Pattern
-1) keyboard in put 'a', switch to StateA
-2) keyboard in put 'b', switch to StateB
+1) keyboard input 'a', switch to StateA
+2) keyboard input 'b', switch to StateB
 3) Ctrl+C exit program
 '''
 
 class State(object):
-    def __init__(self):
-        pass
-    def handle(self):
-        pass
+    def __init__(self): pass
+    def handle(self): pass
+
 
 class StateA(State):
     def __init__(self):
         pass
     def handle(self, context):
-        print('A') 
+        print('StateA') 
         s = input()
         if s == 'b':
             context.state = StateB()
         return
 
+
 class StateB(State):
     def __init__(self):
         pass
     def handle(self, context):
-        print('B') 
+        print('StateB') 
         s = input()
         if s == 'a':
             context.state = StateA()
         return
         
+
 class Context(object):
     def __init__(self, state):
         self.state = state
@@ -39,7 +41,10 @@ class Context(object):
     def operation(self):
         self.state.handle(self)
 
+
 def main():
+    print(MESSAGE)
+
     init = StateA()
     c = Context(init)
 
