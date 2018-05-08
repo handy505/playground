1ed470f2f85184c4416535a5525017617b616d70
handy <handy505@gmail.com>
2018-05-08 17:05:45 +0800

    dev ps3bt car

--- arduino/ps3bt_car/PS3BT/PS3BT.ino
+++ arduino/ps3bt_car/PS3BT/PS3BT.ino
@@ -22,22 +22,58 @@ BTD Btd(&Usb); // You have to create the Bluetooth Dongle instance like so
 PS3BT PS3(&Btd); // This will just create the instance
 //PS3BT PS3(&Btd, 0x00, 0x15, 0x83, 0x3D, 0x0A, 0x57); // This will also store the bluetooth address - this can be obtained from the dongle when running the sketch
 
-bool printTemperature, printAngle;
+
 
 #define SENSITIVE 3
 
-int leftwheel = 0;
-int rightwheel = 0;
-unsigned char leftpwm = 0;
-unsigned char rightpwm = 0;
+#define IN1_PIN 2
+#define IN2_PIN 3
+#define IN3_PIN 4
+#define IN4_PIN 7
+#define ENA_PIN 5
+#define ENB_PIN 6
+
+
 unsigned long timebase = 0;
+unsigned char lefttarget = 0;
+unsigned char leftpwm = 0;
+unsigned char righttarget = 0;
+unsigned char rightpwm = 0;
 unsigned long leftstop_timestamp = 0;
 unsigned long rightstop_timestamp = 0;
 
-unsigned char calc_pwm(unsigned long last_timestamp){
-  int diff = (millis() - last_timestamp) / 4;
-  if (diff > 255) return 255;
-  else            return diff;
+unsigned char calc_pwm2(unsigned long last_timestamp){
+  int diff_time = (millis() - last_timestamp) / 4;
+  int duty = diff_time;
+  if (duty > 255) return 255;
+  else            return duty;
+}
+
+unsigned int calc_pwm(unsigned int value){
+  int delta = 0;
+  if (value > 128)      delta = value - 128;
+  else if(value < 127)  delta = 127 - value;
+  return delta * 2;
+}
+
+void left_forward(void){
+  digitalWrite(IN1_PIN, HIGH);
+  digitalWrite(IN2_PIN, LOW);  
+}
+
+void left_backward(void){
+  digitalWrite(IN1_PIN, LOW);
+  digitalWrite(IN2_PIN, HIGH);
+}
+
+void right_forward(void){
+  digitalWrite(IN3_PIN, HIGH);
+  digitalWrite(IN4_PIN, LOW);    
+}
+
+void right_backward(void){
+  digitalWrite(IN3_PIN, LOW);
+  digitalWrite(IN4_PIN, HIGH);  
 }
 
 void polling(void){
@@ -45,60 +81,89 @@ void polling(void){
   if (PS3.PS3Connected || PS3.PS3NavigationConnected) {
 
     int left = PS3.getAnalogHat(LeftHatY);
+    int right = PS3.getAnalogHat(RightHatY);
+
+    bool active = false;
+    
     if (left < (127 - SENSITIVE)){
       // left forward
-      leftpwm = calc_pwm(leftstop_timestamp);
-      Serial.print(F("\r\n left forward: "));
-      Serial.print(leftpwm);
+      left_forward();
+      
+      lefttarget = calc_pwm(left);
+      if (leftpwm > lefttarget) leftpwm--;
+      else                      leftpwm++;
+      analogWrite(ENA_PIN, leftpwm);
+      active = true;
+      
     }else if (left > (127 + SENSITIVE)){
       // left backward
-      leftpwm = calc_pwm(leftstop_timestamp);
-      Serial.print(F("\r\n left backward: "));
-      Serial.print(leftpwm);      
+      left_backward();
+      
+      lefttarget = calc_pwm(left);
+      if (leftpwm > lefttarget) leftpwm--;
+      else                      leftpwm++;
+      analogWrite(ENA_PIN, leftpwm);
+      active = true;
       
     }else{
-      // left stop
-      leftstop_timestamp = millis();
-      if (leftpwm > 2){
-        leftpwm -= 2;
-        Serial.print(F("\r\n left forward: "));
-        Serial.print(leftpwm);
-      }else{
-        leftpwm = 0;  
-      }
+
+      if(leftpwm > 20)      leftpwm -= 3;
+      else if(leftpwm > 0)  leftpwm -= 1;
+      analogWrite(ENA_PIN, leftpwm);
       
     }
 
-    int right = PS3.getAnalogHat(RightHatY);
     if (right < (127 - SENSITIVE)){
       // right forward
-      rightpwm = calc_pwm(rightstop_timestamp);
-      Serial.print(F("\r\n right forward: "));
-      Serial.print(rightpwm); 
+      right_forward();
+      
+      righttarget = calc_pwm(right);
+      if (rightpwm > righttarget) rightpwm--;
+      else                        rightpwm++;
+      analogWrite(ENB_PIN, rightpwm); 
+      active = true;
             
     }else if (right > (127 + SENSITIVE)){
       // right backward
-      rightpwm = calc_pwm(rightstop_timestamp);
-      Serial.print(F("\r\n right backward: "));
-      Serial.print(rightpwm); 
+      right_backward();
+      
+      righttarget = calc_pwm(right);
+      if (rightpwm > righttarget) rightpwm--;
+      else                        rightpwm++;
+      analogWrite(ENB_PIN, rightpwm); 
+      active = true;
       
     }else{
-      // right stop
-      rightstop_timestamp = millis();
-      if (rightpwm > 2){
-        rightpwm -= 2 ;
-        Serial.print(F("\r\n right forward: "));
-        Serial.print(rightpwm);
-      }else{
-        rightpwm = 0;  
-      }
       
+      if(rightpwm > 20)      rightpwm -= 3;
+      else if(rightpwm > 0)  rightpwm -= 1;
+      analogWrite(ENB_PIN, rightpwm);
+            
+    }
+
+    
       
-    }  
+
+    if (active){
+      Serial.print(F("\r\n "));
+      Serial.print(leftpwm); 
+      Serial.print(F("\t "));
+      Serial.print(rightpwm); 
+      active = false;  
+    }
+    
   }
 }
+
 //-----------------------------
 void setup() {
+  pinMode(IN1_PIN, OUTPUT);
+  pinMode(IN2_PIN, OUTPUT);
+  pinMode(IN3_PIN, OUTPUT);
+  pinMode(IN4_PIN, OUTPUT);
+  pinMode(ENA_PIN, OUTPUT);
+  pinMode(ENB_PIN, OUTPUT);
+  
   Serial.begin(115200);
 #if !defined(__MIPSEL__)
   while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
@@ -118,21 +183,8 @@ void loop() {
   Usb.Task();
 
   if (millis() - timebase > 10){
-    
     polling();
-    
     timebase = millis();  
   }
-
-
-
-#if 0 // Set this to 1 in order to see the angle of the controller
-    if (printAngle) {
-      Serial.print(F("\r\nPitch: "));
-      Serial.print(PS3.getAngle(Pitch));
-      Serial.print(F("\tRoll: "));
-      Serial.print(PS3.getAngle(Roll));
-    }
-#endif
 
 }
