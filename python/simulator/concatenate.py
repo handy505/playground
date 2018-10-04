9789c4a152b96a764260e4e0ad70494474590ef8
handy <handy8831505@gmail.com>
2018-10-04 18:21:24 +0800

    add memory mapping

---
+++ python/simulator/concatenate.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import time
+import threading
+import serial
+
+
+class ConcatThread(threading.Thread):
+    def __init__(self, pvgroup=None, imeter=None, tmeter=None):
+        threading.Thread.__init__(self)       
+        self.pvgroup = pvgroup
+        self.imeter = imeter
+        self.tmeter = tmeter
+        self.ser = serial.Serial(port='/dev/ttyUSB1', baudrate=9600)
+        self.looping = True
+
+
+    def run(self):
+        rxlen = 0
+        mode = 'idle'
+        checktime = time.time() 
+        while self.looping:
+            if mode == 'idle':
+                if self.ser.in_waiting > rxlen:
+                    rxlen = self.ser.in_waiting
+                    checktime = time.time()
+                    mode = 'busy'
+            elif mode == 'busy':
+                TIMEOUT_CONDITION = 0.05
+                if time.time() - checktime > TIMEOUT_CONDITION:
+                    mode = 'timeout'
+                else:
+                    if self.ser.in_waiting > rxlen:
+                        rxlen = self.ser.in_waiting
+                        checktime = time.time()
+            elif mode == 'timeout':
+                rxpacket = self.ser.read(self.ser.in_waiting)
+                self.processing(rxpacket)
+                mode = 'idle'
+                rxlen = 0
+            time.sleep(0.01)
+
+
+    def processing(self, rxpacket):
+        #s = ' '.join(['{:02x}'.format(b) for b in rxpacket]) # debug
+        #print('{:.3f}: {}'.format(time.time(), s))
+
+        for i, pv in enumerate(self.pvgroup.pool):
+            try:
+                #txpacket = pv.response_jbus_query_packet(rxpacket)
+                txpacket = pv.read_memory_by_jbus(rxpacket)
+                if txpacket:
+                    self.ser.write(txpacket)
+                    self.ser.flush()
+                    return
+            except ValueError as ex: 
+                pass
+
+        #txpacket = self.imeter.response_jbus_query(rxpacket)
+        txpacket = self.imeter.read_memory_by_jbus(rxpacket)
+        if txpacket:
+            self.ser.write(txpacket)
+            self.ser.flush()
+            return
+
+        #txpacket = self.tmeter.response_jbus_query(rxpacket)
+        txpacket = self.tmeter.read_memory_by_jbus(rxpacket)
+        if txpacket:
+            self.ser.write(txpacket)
+            self.ser.flush()
+            return
--- python/simulator/main.py
+++ python/simulator/main.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import time
+import threading
+
+import crc
+import memorymapping
 
 class Inverter(object):
     def __init__(self, id):
+        self.lock = threading.RLock()
         self.id = id
         self.alarm = 0x00000000
         self.error = 0x00000000
         self.reflash_timestamp = time.time()
         self.TotalOutputPower = 0
+        self.mm = memorymapping.MemoryMapping(0xc000, 0xc020)
+        for i, val in enumerate(self.mm.memory):
+            self.mm.memory[i] = i
 
     def __str__(self):
         return 'Inverter-{}, {} KW, {} KWH'.format(
@@ -21,16 +29,71 @@ class Inverter(object):
         self.OutputPower = (5000/3600)*diff
         self.TotalOutputPower += self.OutputPower
 
+    def read_memory_by_jbus(self, querypacket):
+        with self.lock:
+            if len(querypacket) < 6:
+                return None
+
+            slavenumber = querypacket[0]
+            functioncode = querypacket[1]
+            address = (querypacket[2] << 8) + querypacket[3]
+            wordlength = querypacket[5]
+
+            # id check
+            if slavenumber != self.id:
+                print('ID error')
+                return None
+
+            # crc check
+            if not crc.check_crc16(querypacket):
+                print('CRC error')
+                return None
+
+            # address check
+            if not address >= self.mm.start_address:
+                print('address error')
+                return None
+            if not ((address+wordlength) <= self.mm.end_address):
+                print('address error')
+                return None
+
+            # prepare response
+            resphex = '{:02x} {:02x} {:02x}'.format(
+                slavenumber, 
+                functioncode, 
+                wordlength*2)
+            resp = bytearray.fromhex(resphex)
+            startindex = (address - self.mm.start_address) * 2
+            endindex = startindex + (wordlength*2) 
+            data = self.mm.dump_from(address, wordlength)
+            d = ' '.join(['{:04x}'.format(b) for b in data])
+            data = bytearray.fromhex(d)
+
+            resp = resp + data
+            resp = crc.append_crc16(resp)
+            return resp
+
 
 def main():
     inv = Inverter(1)
 
-
-
-    for _ in range(1, 10):
+    '''for _ in range(1, 10):
         inv.reflash()
         print(inv)
         time.sleep(1)
+        '''
+
+
+    q = '01 03 c0 00 00 02'
+    q = bytearray.fromhex(q)
+    q = crc.append_crc16(q)
+    print('query: {}'.format(q))
+
+    r = inv.read_memory_by_jbus(q)
+    print('return: {}'.format(r))
+
+
+
 
 
 if __name__ == '__main__':
