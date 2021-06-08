6e7750bf87f9c533286799e21eb1c1d1a8aed999
handy <handy8831505@gmail.com>
2021-06-08 02:10:44 -0700

    mvc3, alarm clock, ok

--- python/pyqt/mvc3/main.py
+++ python/pyqt/mvc3/main.py
@@ -2,37 +2,46 @@ from PyQt5 import QtWidgets
 from ui import Ui_MainWindow
 import sys
 
-from datetime import datetime
+import datetime
 import threading
 import time
-
 
 
 class Model(threading.Thread):
     def __init__(self):
         super().__init__()
         self.observer = None 
-
-        self.num = 0
-        self.now = datetime.now()
-
-
-    def increase_num(self):
-        self.num += 1
-        self.notify()
+        self.now = datetime.datetime.now()
+        self.alarmtime = None
+        self.countdown = None
 
 
     def notify(self):
         if self.observer:
             self.observer.update(self)
 
+
+    def set_alarm_time(self, hour, minute):
+        self.alarmtime = datetime.time(hour=hour, minute=minute)
+        self.notify()
+
+
+    def update_countdown(self):
+        if self.alarmtime:
+            alarmdt = datetime.datetime.combine(datetime.date.today(), self.alarmtime)
+            self.countdown = alarmdt - self.now
+
+
     def run(self):
         while True:
-
-            self.now = datetime.now()
+            self.now = datetime.datetime.now()
+            self.update_countdown()
             self.notify()
             time.sleep(1)
 
+
+# ---------------------------------------------------------------
+# viewer
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
@@ -42,11 +51,16 @@ class MainWindow(QtWidgets.QMainWindow):
 
 
     def update(self, subject):
-        self.ui.label.setText(str(subject.num))
-        self.ui.label_2.setText(str(subject.now))
+        self.ui.label.setText(str(subject.now))
+
+        alarmtime = str(subject.alarmtime)
+        self.ui.label_2.setText(alarmtime)
+
+        countdown = str(subject.countdown)
+        self.ui.label_3.setText(countdown)
 
 
-
+# ---------------------------------------------------------------
 class Controller(object):
     def __init__(self):
         self.view = MainWindow()
@@ -55,14 +69,22 @@ class Controller(object):
         self.model.observer = self.view
         self.model.start()
 
-        self.view.ui.pushButton.clicked.connect(self.buttonClicked)
+        self.view.ui.setButton.clicked.connect(self.setAlarmTime)
+        self.view.ui.clearButton.clicked.connect(self.clearAlarmTime)
         self.view.show()
 
-    def buttonClicked(self):
-        #now = datetime.now()
-        #self.view.ui.label.setText(str(now))
-        
-        self.model.increase_num()
+
+    def setAlarmTime(self):
+        hour = self.view.ui.timeEdit.time().hour()
+        minute = self.view.ui.timeEdit.time().minute()
+        self.model.set_alarm_time(hour, minute)
+
+
+    def clearAlarmTime(self):
+        self.model.alarmtime = None
+        self.model.countdown = None
+        self.view.ui.label_2.clear()
+        self.view.ui.label_3.clear()
 
 
 if __name__ == '__main__':
